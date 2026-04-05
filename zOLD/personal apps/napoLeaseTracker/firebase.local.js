1b65e35ea642653910272d91fd67656f75e6281f
elihermes <elihermes@gmail.com>
2026-04-05 17:47:23 +0300

    Remove local generated files from tracking

--- zOLD/personal apps/napoLeaseTracker/firebase.local.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyBC22UFlSX9jXTd1CIWxyUcbJtw5oVUkpY',
-  authDomain: 'napo-lease-tracker.firebaseapp.com',
-  projectId: 'napo-lease-tracker',
-  storageBucket: 'napo-lease-tracker.firebasestorage.app',
-  messagingSenderId: '903051434298',
-  appId: '1:903051434298:web:1e34220e306ab1844e9803',
-  measurementId: 'G-RE9GNNMT6Z'
-}; 
